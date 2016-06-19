a4bb43beca0316fa3a689d41b994a14aaa9d5a69
forente <zerofox16@gmail.com>
2016-06-18 21:24:59 -0500

    Reveal Resume button
    shows and hides resume at the press of a button

--- sample.js
+++ sample.js
@@ -1,1 +1,38 @@
 console.log("Success!!");
+
+
+
+function showResume(){
+	
+	var resumeVisible = false; 
+
+	$("header").hide();
+	$('main').hide();
+	
+	
+	$('button').on('click', function(){
+		
+		if (!resumeVisible){
+			
+			$("header").show();
+			$('main').show();
+			$('button').text('Remove Resume');
+			resumeVisible = true;
+		}
+		
+		else{
+			$("header").hide();
+			$('main').hide();
+			$('button').text('Reveal Resume');
+			resumeVisible = false; 
+		}
+		
+	});
+	
+
+
+}
+
+
+
+$(document).ready(showResume);
